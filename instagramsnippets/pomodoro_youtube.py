83b65eae0c4698d41bc5633a7e8326f755a964bc
Ali Khundmiri <salikhundmiri@gmail.com>
2019-05-22 13:14:22 +0530

    updated the code to work at start, and at the end

--- instagramsnippets/pomodoro_youtube.py
+++ instagramsnippets/pomodoro_youtube.py
@@ -1,4 +1,4 @@
-import os, webbrowser, random, time
+import sys, os, webbrowser, random, time
 import video_details
 video_ids = [
 '3-NTv0CdFCk','ybUsle7TzGk',
@@ -27,6 +27,30 @@ def play_video(video_id):
 	webbrowser.open(youtube_url)
 
 if __name__ == '__main__':
-	video_id = pick_video()
-	notify("Pomodoro Youtube | By Ali Khundmiri", 'Playing A Song in 3 seconds...', 'Break Time')
-	play_video(video_id)
+
+	if len(sys.argv[1]) > 1:
+		artist_name = sys.argv[1]
+	else:
+		artist_name = input("Enter the Artist Name: ")
+
+	try:
+		input_prompt = sys.argv[1]
+		if input_prompt == 'start':
+			video_id = pick_video()
+			notify("Pomodoro Youtube | By Ali Khundmiri", 'Playing A Song in 3 seconds...', 'Break Time')
+			play_video(video_id)
+		elif input_prompt == 'stop':
+			notify("Pomodoro Youtube | By Ali Khundmiri", 'Times up, Give your 100 percent. All the best.', 'Back to work')
+
+	except IndexError as e:
+		print("no platform gives as input")
+		video_id = pick_video()
+		notify("Pomodoro Youtube | By Ali Khundmiri", 'Playing A Song in 3 seconds...', 'Break Time')
+		play_video(video_id)
+'''
+
+CRONJOB
+25,55 11-15 * * 1-5  /Library/Frameworks/Python.framework/Versions/3.5/bin/python3.5 /Users/alikhundmiri/Desktop/pythons/everything/instagramsnippets/pomodoro_youtube.py start
+
+30,0 11-15 * * 1-5 /Library/Frameworks/Python.framework/Versions/3.5/bin/python3.5 /Users/alikhundmiri/Desktop/pythons/everything/instagramsnippets/pomodoro_youtube.py stop
+'''
